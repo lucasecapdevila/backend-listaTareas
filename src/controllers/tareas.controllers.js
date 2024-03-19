774dc934bcc0c05efa5563494f69ac026a0da852
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-18 23:55:41 -0300

    agrego la lógica para devolver el array de tareas

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,8 +1,15 @@
 import Tarea from "../database/models/tarea.js";
 
-export const listarTareas = (req, res) => {
-  console.log('Preparo la lista de tareas');
-  res.send('Lista de tareas enviada')
+export const listarTareas = async(req, res) => {
+  try {
+    const listaTareas = await Tarea.find()
+    res.status(200).json(listaTareas)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      mensaje: 'Se produjo un error al buscar las tareas'
+    })
+  }
 }
 
 export const crearTarea = async(req, res) => {
