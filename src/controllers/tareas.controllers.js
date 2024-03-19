63be7c511cc70aaeef6c7c98914a3f510dde9bf4
Lucas Capdevila <lcapdevila60@gmail.com>
2024-03-18 23:38:52 -0300

    agrego la lógica para crear una tarea

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,4 +1,22 @@
+import Tarea from "../database/models/tarea.js";
+
 export const listarTareas = (req, res) => {
   console.log('Preparo la lista de tareas');
   res.send('Lista de tareas enviada')
+}
+
+export const crearTarea = async(req, res) => {
+  try {
+    const nuevaTarea = new Tarea(req.body)
+    await nuevaTarea.save()
+    
+    res.status(201).json({
+      mensaje: 'La tarea fue creada exitosamente.'
+    })
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      mensaje: 'La tarea no pudo ser creada'
+    })
+  }
 }
